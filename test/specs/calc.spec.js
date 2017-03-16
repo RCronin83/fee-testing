940fd06a92a675e59caf432d5516231df1837fa8
Ryan Cronin <Ryan@Ryans-MBP.home>
2017-03-15 22:29:04 -0400

    5 tests working for first function, 2 for second function

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -1,10 +1,47 @@
 (function() {
     'use strict';
+    let expect = window.chai.expect;
+
+    describe('calc module', function(){
+
+      describe('sum function', function(){
+        it('should give the sum when provided two numbers', function(){
+          let result = window.calc.sum([1,2]);
+          expect(result).to.be.a('number').and.to.equal(3);
+        });
+        it('should handle no arguments', function(){
+          let result = window.calc.sum();
+          expect(result).to.equal(0);
+        });
+        it('should handle only 1 argument in an Array', function(){
+          let result = window.calc.sum([10]);
+          expect(result).to.be.a('number').and.to.equal(10);
+        });
+        it('should handle only 1 argument', function(){
+          let result = window.calc.sum(10);
+          expect(result).to.be.NaN;
+        });
+        it('should convert strings to numbers for addition', function(){
+          let result = window.calc.sum(['5', '10']);
+          expect(result).to.be.a('number').and.to.equal(15);
+        });
+      });
 
 
-    // TODO: Your tests go here!
-
-    
-
-
+      describe('factorial function', function(){
+        it('returns the factorial for the given number', function(){
+          let result = window.calc.factorial(5);
+          expect(result).to.be.a('number').and.to.equal(120);
+        });
+        it('should handle no argument', function(){
+          let result = window.calc.factorial();
+          expect(result).to.equal(0);
+        });
+        // it('should convert a string to a number for factoring', function(){
+        //   let result = window.calc.factorial('five');
+        //   expect(result).to.be.a('number').and.to.equal(120);
+        //
+        // });
+      });
+    });
 })();
